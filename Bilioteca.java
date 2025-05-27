05cb76d1a0281a267c78b7ebf0ef1aa3ac753e3c
DevRafaelLuz <rafael.luz2005@gmail.com>
2025-05-27 19:34:01 -0300

    Removido classe desnecessária

--- Bilioteca.java
+++
@@ -1,9 +0,0 @@
-
-public class Bilioteca {
-    public static void main(String[] args) {
-
-        System.out.printf("Ola, bem vindo a biblioteca!");
-        //teste de funcionamento de commit . . . .
-
-    }
-}
