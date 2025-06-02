a4c5425991a4623b4d6474f0678a48c521a32da8
josevictorbdbr <josevictorbdbr@gmail.com>
2025-06-02 11:59:01 -0300

    Adicionado base do arquivo Main

--- biblioteca/src/main/java/com/github/biblioteca/Main.java
+++ biblioteca/src/main/java/com/github/biblioteca/Main.java
@@ -2,6 +2,14 @@ package biblioteca.src.main.java.com.github.biblioteca;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+            System.out.println("\n------ MENU PRINCIPAL------");
+            System.out.println("1. Adicionar usuario");
+            System.out.println("2. Listar usuarios matriculados");
+            System.out.println("3. Adicionar livro");
+            System.out.println("4. Listar livros disponiveis");
+            System.out.println("0. Sair");
+            System.out.print("Opcao: ");
+
     }
 }
