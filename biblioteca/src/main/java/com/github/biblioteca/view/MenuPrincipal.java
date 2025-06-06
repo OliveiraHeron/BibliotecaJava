7a3fb3eb33528c50fa1d0fff74fe6713646bc606
josevictorbdbr <josevictorbdbr@gmail.com>
2025-06-06 20:22:43 -0300

    integrado EmprestimoView ao MenuPrincipal

--- biblioteca/src/main/java/com/github/biblioteca/view/MenuPrincipal.java
+++ biblioteca/src/main/java/com/github/biblioteca/view/MenuPrincipal.java
@@ -1,10 +1,8 @@
 package com.github.biblioteca.view;
 
 import java.util.Scanner;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import com.github.biblioteca.interfaces.Menu;
 
 public class MenuPrincipal implements Menu {
@@ -12,24 +10,27 @@ public class MenuPrincipal implements Menu {
     private static Scanner scanner = new Scanner(System.in);
     private static UsuarioView usuarioView = new UsuarioView();
     private static LivroView livroView = new LivroView();
+    private static EmprestimoView emprestimoView = new EmprestimoView(); // NOVO
 
     @Override
     public void exibirMenu() {
         logger.info("Iniciando o sistema de biblioteca...");
         while (true) {
-            System.out.println("\n------ MENU PRINCIPAL------");
-            System.out.println("1. Gerenciamente de Usuarios");
+            System.out.println("\n------ MENU PRINCIPAL ------");
+            System.out.println("1. Gerenciamento de Usuarios");
             System.out.println("2. Gerenciamento de Livros");
+            System.out.println("3. Gerenciamento de Emprestimos"); // NOVO
             System.out.println("0. Sair");
             System.out.print("Opcao: ");
             int op = scanner.nextInt();
+            scanner.nextLine(); // limpar buffer
 
             switch (op) {
                 case 1 -> usuarioView.exibirMenu();
                 case 2 -> livroView.exibirMenu();
+                case 3 -> emprestimoView.exibirMenu(); // NOVO
                 case 0 -> {
                     System.out.println("Encerrando programa...");
-                    scanner.close();
                     logger.info("Sistema de biblioteca encerrado.");
                     return;
                 }
