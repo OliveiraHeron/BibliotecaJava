386df4655cee95daad8d551adba115c311c2e6b3
josevictorbdbr <josevictorbdbr@gmail.com>
2025-06-06 20:20:42 -0300

    Adicionado classe Emprestimo com relacao Usuario e Livro

--- biblioteca/src/main/java/com/github/biblioteca/models/Emprestimo.java
+++ biblioteca/src/main/java/com/github/biblioteca/models/Emprestimo.java
@@ -1,1 +1,41 @@
 package com.github.biblioteca.models;
+
+import java.time.LocalDate;
+
+public class Emprestimo {
+    private Usuario usuario;
+    private Livro livro;
+    private LocalDate dataEmprestimo;
+    private LocalDate dataDevolucao;
+
+    public Emprestimo(Usuario usuario, Livro livro) {
+        this.usuario = usuario;
+        this.livro = livro;
+        this.dataEmprestimo = LocalDate.now();
+        this.dataDevolucao = dataEmprestimo.plusDays(7);
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Usuario: " + usuario.getNome() +
+                " | Livro: " + livro.getTitulo() +
+                " | Emprestado em: " + dataEmprestimo +
+                " | Devolucao ate: " + dataDevolucao);
+    }
+
+    // Getters
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public LocalDate getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public LocalDate getDataDevolucao() {
+        return dataDevolucao;
+    }
+}
