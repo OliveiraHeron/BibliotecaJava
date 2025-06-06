9e09dce236fad80f37006fd9e84613c375559e0e
DevRafaelLuz <rafael.luz2005@gmail.com>
2025-06-06 09:15:13 -0300

    Menu principal ajustado

--- biblioteca/src/main/java/com/github/biblioteca/Main.java
+++ biblioteca/src/main/java/com/github/biblioteca/Main.java
@@ -1,44 +1,9 @@
 package com.github.biblioteca;
 
-import java.util.Scanner;
-
-import com.github.biblioteca.controllers.LivroController;
-import com.github.biblioteca.controllers.UsuarioController;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.github.biblioteca.view.MenuPrincipal;
 
 public class Main {
-    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        UsuarioController usuarioCtrl = new UsuarioController();
-        LivroController livroCtrl = new LivroController();
-        Scanner scanner = new Scanner(System.in);
-
-        logger.info("Iniciando o sistema de biblioteca...");
-
-        while (true) {
-            System.out.println("\n------ MENU PRINCIPAL------");
-            System.out.println("1. Adicionar usuario");
-            System.out.println("2. Listar usuarios matriculados");
-            System.out.println("3. Adicionar livro");
-            System.out.println("4. Listar livros disponiveis");
-            System.out.println("0. Sair");
-            System.out.print("Opcao: ");
-            int op = Integer.parseInt(scanner.nextLine());
-
-            switch (op) {
-                case 1 -> usuarioCtrl.adicionarUsuario();
-                case 2 -> usuarioCtrl.listarUsuarios();
-                case 3 -> livroCtrl.adicionarLivro();
-                case 4 -> livroCtrl.listarLivros();
-                case 0 -> {
-                    System.out.println("Encerrando programa...");
-                    scanner.close();
-                    logger.info("Sistema de biblioteca encerrado.");
-                    return;
-                }
-                default -> System.out.println("--ERRO--: Opção invalida");
-            }
-        }        
+        MenuPrincipal.ExibirMenu();                
     }
 }
---
+++ biblioteca/src/main/java/com/github/biblioteca/view/MenuPrincipal.java
@@ -0,0 +1,43 @@
+package com.github.biblioteca.view;
+
+import java.util.Scanner;
+
+import com.github.biblioteca.controllers.LivroController;
+import com.github.biblioteca.controllers.UsuarioController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MenuPrincipal {
+    private static final Logger logger = LogManager.getLogger(MenuPrincipal.class);
+    private static Scanner scanner = new Scanner(System.in);
+    private static UsuarioController usuarioCtrl;
+    private static LivroController livroCtrl;
+
+    public static void ExibirMenu() {
+        logger.info("Iniciando o sistema de biblioteca...");
+        while (true) {
+            System.out.println("\n------ MENU PRINCIPAL------");
+            System.out.println("1. Adicionar usuario");
+            System.out.println("2. Listar usuarios matriculados");
+            System.out.println("3. Adicionar livro");
+            System.out.println("4. Listar livros disponiveis");
+            System.out.println("0. Sair");
+            System.out.print("Opcao: ");
+            int op = scanner.nextInt();
+
+            switch (op) {
+                case 1 -> usuarioCtrl.adicionarUsuario();
+                case 2 -> usuarioCtrl.listarUsuarios();
+                case 3 -> livroCtrl.adicionarLivro();
+                case 4 -> livroCtrl.listarLivros();
+                case 0 -> {
+                    System.out.println("Encerrando programa...");
+                    scanner.close();
+                    logger.info("Sistema de biblioteca encerrado.");
+                    return;
+                }
+                default -> System.out.println("--ERRO--: Opção invalida");
+            }
+        }
+    }
+}
