5493b921e11e8c3ba7dc2487e122e1bd6c5102c4
DevRafaelLuz <rafael.luz2005@gmail.com>
2025-06-07 17:01:30 -0300

    Aplicando alguns ajustes na classe Funcionario

--- biblioteca/src/main/java/com/github/biblioteca/models/Funcionario.java
+++ biblioteca/src/main/java/com/github/biblioteca/models/Funcionario.java
@@ -1,67 +1,63 @@
-    package com.github.biblioteca.models;
+package com.github.biblioteca.models;
 
-    public class Funcionario {
-        private String nome;
-        private String cpf;
-        private int idade;
-        private String matricula;
-        private String cargo;
+public class Funcionario extends Pessoa {
+    private String matricula;
+    private String cargo;
 
-        public Funcionario(String nome, String cpf, int idade, String matricula, String cargo) {
-            this.nome = nome;
-            this.cpf = cpf;
-            this.idade = idade;
-            this.matricula = matricula;
-            this.cargo = cargo;
-        }
-
-        public void exibirInformacoes() {
-            System.out.println("Nome: " + nome);
-            System.out.println("CPF: " + cpf);
-            System.out.println("Idade: " + idade);
-            System.out.println("Matrícula: " + matricula);
-            System.out.println("Cargo: " + cargo);
-        }
-
-        // Getters e Setters
-
-        public String getNome() {
-            return nome;
-        }
-
-        public void setNome(String nome) {
-            this.nome = nome;
-        }
-
-        public String getCpf() {
-            return cpf;
-        }
-
-        public void setCpf(String cpf) {
-            this.cpf = cpf;
-        }
-
-        public int getIdade() {
-            return idade;
-        }
-
-        public void setIdade(int idade) {
-            this.idade = idade;
-        }
-
-        public String getMatricula() {
-            return matricula;
-        }
-
-        public void setMatricula(String matricula) {
-            this.matricula = matricula;
-        }
-
-        public String getCargo() {
-            return cargo;
-        }
-
-        public void setCargo(String cargo) {
-            this.cargo = cargo;
-        }
+    public Funcionario(String nome, String cpf, int idade, String matricula, String cargo) {
+        super(nome, cpf, idade);
+        this.matricula = matricula;
+        this.cargo = cargo;
     }
+
+    @Override
+    public void exibirInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Idade: " + idade);
+        System.out.println("Matrícula: " + matricula);
+        System.out.println("Cargo: " + cargo);
+    }
+
+    // Getters e Setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
